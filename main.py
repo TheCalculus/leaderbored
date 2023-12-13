ab7b05946ee881d2f9f93c39fca853569e47a401
thecalculus <jaisalbhasinis@gmail.com>
2023-12-13 20:10:05 +1100

    first commit, working* theme loader, basic website layout (no backend)

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+THEMER_DEFAULT_DIRECTORY = "static/css/themes"
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/board/<id>")
+def board(id):
+    # query database for id
+    # request authentification if required
+    return render_template('leaderboard.html')
+
+@app.context_processor
+def inject_global_variables():
+    name = "serika_dark"
+    return dict(name=name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
